696ae3713e6b0caff163578d431f2d4f6a91bdee
kenx00x <kenx00x@gmail.com>
2022-10-19 23:22:54 +0200

    remove virtualgrasp

--- Assets/Scripts/TeleportationHandler.cs
+++ Assets/Scripts/TeleportationHandler.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class TeleportationHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject player_rig;
+    private bool is_aiming = false;
+    private GameObject currentDestination;
+    public GameObject destination;
     void Start()
     {
-        
+        currentDestination = Instantiate(destination, transform.position, Quaternion.identity);
     }
+    public void SwitchAiming()
+    {
+        is_aiming = !is_aiming;
 
-    // Update is called once per frame
+        if (!is_aiming)
+        {
+            currentDestination.SetActive(false);
+        }
+    }
     void Update()
     {
-        
+        if (is_aiming)
+        {
+            CheckForDestination();
+        }
+    }
+
+    private void CheckForDestination()
+    {
+        Ray ray = new Ray(transform.position, transform.rotation * Vector3.up);
+
+        RaycastHit hit;
+
+        bool b = Physics.Raycast(ray, out hit, 5f, 1 << 9);
+        if (b)
+        {
+            currentDestination.transform.position = hit.point;
+            currentDestination.SetActive(true);
+        }
+    }
+
+    public void Teleport()
+    {
+        if (is_aiming && currentDestination.activeSelf)
+        {
+            player_rig.transform.position = currentDestination.transform.position;
+            currentDestination.SetActive(false);
+        }
     }
 }
